cdbab746d0260d575dd606bd5ca190bd7a1b6840
ystSaiteja <ystsaiteja@gmail.com>
2024-09-27 15:04:02 +0530

    you can type any message you want here

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 export default async function Page() {
   let data = await fetch("https://api.vercel.app/blog");
   let posts = await data.json();
-  return (
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  );
+  return <h1>Hello</h1>;
 }
